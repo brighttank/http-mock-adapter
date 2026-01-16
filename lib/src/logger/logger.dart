cd607da4c6cfea978b65cb6fbe53dbfaf02bb4c7
Andrew Goldsmith <andrew@brighttank.net>
2026-01-16 15:53:15 +0700

    remove pretty printer from logger

--- lib/src/logger/logger.dart
+++ lib/src/logger/logger.dart
@@ -3,13 +3,5 @@ import 'package:logger/logger.dart';
 Logger getLogger(bool printLogs) {
   return Logger(
     level: printLogs ? Level.debug : Level.off,
-    printer: PrettyPrinter(
-      methodCount: 0,
-      errorMethodCount: 8,
-      lineLength: 120,
-      colors: true,
-      printEmojis: true,
-      printTime: false,
-    ),
   );
 }
